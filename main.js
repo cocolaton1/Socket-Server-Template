36601ff6427664437e3a44e45e65ba19cf30582f
idontseeany1 <138950265+cocolaton1@users.noreply.github.com>
2023-12-02 13:12:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,69 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const WebSocket = require("ws");
 
 app.use(express.static("public"));
-// require("dotenv").config();
 
 const serverPort = process.env.PORT || 3000;
 const server = http.createServer(app);
-const WebSocket = require("ws");
 
-let keepAliveId;
-
-const wss =
-  process.env.NODE_ENV === "production"
-    ? new WebSocket.Server({ server })
-    : new WebSocket.Server({ port: 5001 });
+const wss = process.env.NODE_ENV === "production"
+            ? new WebSocket.Server({ server })
+            : new WebSocket.Server({ port: 5001 });
 
 server.listen(serverPort);
 console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
+
+let keepAliveId;
 
 wss.on("connection", function (ws, req) {
   console.log("Connection Opened");
   console.log("Client size: ", wss.clients.size);
 
   if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
+    console.log("First connection. Starting keepalive");
     keepServerAlive();
   }
 
   ws.on("message", (data) => {
-    let stringifiedData = data.toString();
-    if (stringifiedData === 'pong') {
-      console.log('keepAlive');
-      return;
+    try {
+      // Chuyển đổi Buffer thành chuỗi
+      const messageString = data.toString();
+
+      // Phân tích cú pháp chuỗi thành JSON (nếu dữ liệu đến là JSON)
+      const messageData = JSON.parse(messageString);
+      console.log('Received JSON data:', messageData);
+
+      // Xử lý dữ liệu JSON và broadcast (nếu cần)
+      broadcast(ws, JSON.stringify(messageData), false);
+    } catch (e) {
+      console.log('Received string data:', data.toString());
+      broadcast(ws, data.toString(), false);
     }
-    broadcast(ws, stringifiedData, false);
   });
 
   ws.on("close", (data) => {
-    console.log("closing connection");
+    console.log("Closing connection");
 
     if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
+      console.log("Last client disconnected, stopping keepAlive interval");
       clearInterval(keepAliveId);
     }
   });
 });
 
-// Implement broadcast function because of ws doesn't have it
+// Implement broadcast function
 const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  }
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN && (includeSelf || client !== ws)) {
+      client.send(message);
+    }
+  });
 };
 
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
+// Sends a ping message to all connected clients every 50 seconds
+const keepServerAlive = () => {
   keepAliveId = setInterval(() => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
@@ -77,7 +73,6 @@ const broadcast = (ws, message, includeSelf) => {
   }, 50000);
 };
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
