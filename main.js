cf72888c771fe2f8562b7bf9ee1e2a6a0935e6b3
idontseeany1 <138950265+cocolaton1@users.noreply.github.com>
2024-06-01 03:13:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,93 +17,17 @@ server.on('upgrade', (request, socket, head) => {
 
 server.listen(PORT);
 
-const usersInChat = new Map();
-const pictureReceivers = new Map(); 
-let keepAliveId;
-
 wss.on("connection", function (ws) {
-    const userID = generateUniqueID();  
-
-    ws.on("message", data => {
-        handleMessage(ws, data, userID);
+    ws.on("message", (data) => {
+        // Forward all signaling messages to other clients
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
     });
 
     ws.on("close", () => {
-        handleDisconnect(userID);
         ws.removeAllListeners(); 
     });
-
-    if (wss.clients.size === 1 && !keepAliveId) {
-        keepServerAlive();
-    }
 });
-
-wss.on("close", () => {
-    clearInterval(keepAliveId);
-    keepAliveId = null;
-});
-
-function generateUniqueID() {
-    return Math.random().toString(36).substr(2, 9);
-}
-
-function handleMessage(ws, data, userID) {
-    try {
-        const messageData = JSON.parse(data);
-        if (messageData.command === 'Picture Receiver') {
-            pictureReceivers.set(userID, ws);
-        } else if (messageData.type === 'screenshot' && messageData.data.startsWith('data:image/png;base64')) {
-            broadcastToPictureReceivers({
-                type: 'screenshot',
-                action: messageData.action,
-                screen: messageData.screen,
-                data: messageData.data
-            });
-        } else if (messageData.action === 'screenshot_result') {
-            broadcastToPictureReceivers({
-                type: 'screenshot',
-                action: messageData.action,
-                screen: messageData.screen,
-                data: messageData.data
-            });
-        } else {
-            broadcastToAllExceptPictureReceivers(ws, JSON.stringify(messageData), true);
-        }
-    } catch (e) {
-        console.error('Error parsing data:', e);
-    }
-}
-
-function broadcastToPictureReceivers(message) {
-    const data = JSON.stringify(message);
-    pictureReceivers.forEach((ws, userId) => {
-        if (ws.readyState === WebSocket.OPEN) {
-            ws.send(data, error => {
-                if (error) console.error("Error sending message to receiver:", error);
-            });
-        }
-    });
-}
-
-function broadcastToAllExceptPictureReceivers(senderWs, message, includeSelf) {
-    wss.clients.forEach(client => {
-        if (!pictureReceivers.has(client) && client.readyState === WebSocket.OPEN && (includeSelf || client !== senderWs)) {
-            client.send(message);
-        }
-    });
-}
-
-function handleDisconnect(userID) {
-    usersInChat.delete(userID);
-    pictureReceivers.delete(userID);
-}
-
-function keepServerAlive() {
-    keepAliveId = setInterval(() => {
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.ping(); 
-            }
-        });
-    }, 30000);
-}
