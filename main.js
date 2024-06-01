559d86b153f8bba2c476e64ea41bcf8855ed4495
idontseeany1 <138950265+cocolaton1@users.noreply.github.com>
2024-06-02 01:12:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -18,24 +18,23 @@ server.on('upgrade', (request, socket, head) => {
 server.listen(PORT);
 
 const usersInChat = new Map();
-const pictureReceivers = new Map(); 
+const pictureReceivers = new Map();
 let keepAliveId;
 
 wss.on("connection", function (ws) {
-    const userID = generateUniqueID();  
+    const userID = generateUniqueID();
 
     ws.on("message", data => {
-        // Check if data is a buffer (image data)
-        if (Buffer.isBuffer(data)) {
-            handleImageBuffer(ws, data, userID);
-        } else {
+        if (typeof data === 'string') {
             handleMessage(ws, data, userID);
+        } else if (Buffer.isBuffer(data)) {
+            handleBufferMessage(data);
         }
     });
 
     ws.on("close", () => {
         handleDisconnect(userID);
-        ws.removeAllListeners(); 
+        ws.removeAllListeners();
     });
 
     if (wss.clients.size === 1 && !keepAliveId) {
@@ -57,7 +56,14 @@ function handleMessage(ws, data, userID) {
         const messageData = JSON.parse(data);
         if (messageData.command === 'Picture Receiver') {
             pictureReceivers.set(userID, ws);
-        } else if (messageData.type === 'screenshot') {
+        } else if (messageData.type === 'screenshot' && messageData.data.startsWith('data:image/png;base64')) {
+            broadcastToPictureReceivers({
+                type: 'screenshot',
+                action: messageData.action,
+                screen: messageData.screen,
+                data: messageData.data
+            });
+        } else if (messageData.action === 'screenshot_result') {
             broadcastToPictureReceivers({
                 type: 'screenshot',
                 action: messageData.action,
@@ -72,7 +78,7 @@ function handleMessage(ws, data, userID) {
     }
 }
 
-function handleImageBuffer(ws, buffer, userID) {
+function handleBufferMessage(buffer) {
     broadcastBufferToPictureReceivers(buffer);
 }
 
@@ -81,6 +87,17 @@ function broadcastBufferToPictureReceivers(buffer) {
         if (ws.readyState === WebSocket.OPEN) {
             ws.send(buffer, { binary: true }, error => {
                 if (error) console.error("Error sending buffer to receiver:", error);
+            });
+        }
+    });
+}
+
+function broadcastToPictureReceivers(message) {
+    const data = JSON.stringify(message);
+    pictureReceivers.forEach((ws, userId) => {
+        if (ws.readyState === WebSocket.OPEN) {
+            ws.send(data, error => {
+                if (error) console.error("Error sending message to receiver:", error);
             });
         }
     });
@@ -103,7 +120,7 @@ function keepServerAlive() {
     keepAliveId = setInterval(() => {
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.ping(); 
+                client.ping();
             }
         });
     }, 30000);
